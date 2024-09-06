517b2729412f7584cc33ee5b8b77232aca18b2a6
erisk405 <133034175+erisk405@users.noreply.github.com>
2024-09-07 00:02:12 +0700

    RealtimeStatusUI_overview_draf2

--- src/app/(router)/overview/_components/MyEvaluated.tsx
+++ src/app/(router)/overview/_components/MyEvaluated.tsx
@@ -23,22 +23,10 @@ import {
   CardTitle,
 } from "@/components/ui/card";
 import { ChartConfig, ChartContainer } from "@/components/ui/chart";
+import useStore from "@/app/store/store";
+import GlobalApi from "@/app/_unit/GlobalApi";
 
 export const description = "A radial chart with a custom shape";
-
-const chartData = [
-  { browser: "safari", visitors: 1260, fill: "var(--color-safari)" },
-];
-
-const chartConfig = {
-  visitors: {
-    label: "Visitors",
-  },
-  safari: {
-    label: "Safari",
-    color: "hsl(var(--chart-2))",
-  },
-} satisfies ChartConfig;
 
 interface Score {
   id: string;
@@ -54,6 +42,30 @@ const MyEvaluated = () => {
   const [Academicknowledge, setAcademicknowledge] = useState(61);
   const [OperationalSkills, setOperationalSkills] = useState(34);
   const [AffectiveDomain, setAffectiveDomain] = useState(50);
+
+  const { departments } = useStore();
+
+  const myQuest = [
+    {
+      id: "AOU01",
+      name: "คุณประเมินบุคลากรไปแล้ว",
+      chartData: [
+        { browser: "alreadyEvaluated", quantity: 40, fill: "rgb(253 224 71)" },
+      ],
+      engle: 200,
+    },
+    {
+      id: "AOU02",
+      name: "มีคนประเมินคุณทั้งหมด",
+      chartData: [{ browser: "evaluatedYou", quantity: 60, fill: "#60a5fa" }],
+      engle: 300,
+    },
+  ];
+  const chartConfig = {
+    quantity: {
+      label: "Already evaluated",
+    },
+  } satisfies ChartConfig;
 
   let myscore: Score[] = [
     {
@@ -87,150 +99,176 @@ const MyEvaluated = () => {
 
   return (
     <div className="h-full flex flex-col gap-5">
-      <div className=" flex justify-between flex-wrap gap-5">
-        <div className="w-full grid grid-cols-3 gap-5">
-          {[1, 2, 3].map((item, index) => (
-            <Card className="flex flex-col" key={index + "dog"}>
-              <CardHeader className="items-center pb-0">
-                <CardTitle>ประเมิณบุคลากรไปแล้ว</CardTitle>
-                <CardDescription>January - June 2024</CardDescription>
-              </CardHeader>
-              <CardContent className="flex-1 pb-0">
-                <ChartContainer
-                  config={chartConfig}
-                  className="mx-auto aspect-square max-h-[250px]"
+      <div className=" flex flex-col justify-between gap-5">
+        <div className="w-full grid grid-cols-2 gap-5">
+          {myQuest &&
+            myQuest.map((item, index) => (
+              <motion.div
+                key={item.id}
+                initial={{ opacity: 0, y: -100 }}
+                animate={{ opacity: 1, y: 0 }}
+                transition={{
+                  duration: 1.5,
+                  delay: index * 0.1,
+                  ease: [0, 0.71, 0.2, 1.01],
+                }}
+              >
+                <Card className="grid grid-cols-2">
+                  <CardContent className="p-0  h-auto col-span-2">
+                    <CardHeader className="items-start pb-0">
+                      <CardTitle>{item.name}</CardTitle>
+                    </CardHeader>
+                    <div className="w-full flex justify-center">
+                      <ChartContainer
+                        config={chartConfig}
+                        className="aspect-square h-[220px] "
+                      >
+                        <RadialBarChart
+                          data={item.chartData}
+                          endAngle={item.engle}
+                          innerRadius={80}
+                          outerRadius={140}
+                        >
+                          <PolarGrid
+                            gridType="circle"
+                            radialLines={false}
+                            stroke="none"
+                            className="first:fill-muted last:fill-background"
+                            polarRadius={[86, 74]}
+                          />
+                          <RadialBar dataKey={"quantity"} background />
+                          <PolarRadiusAxis
+                            tick={false}
+                            tickLine={false}
+                            axisLine={false}
+                          >
+                            <Label
+                              content={({ viewBox }) => {
+                                if (
+                                  viewBox &&
+                                  "cx" in viewBox &&
+                                  "cy" in viewBox
+                                ) {
+                                  return (
+                                    <text
+                                      x={viewBox.cx}
+                                      y={viewBox.cy}
+                                      textAnchor="middle"
+                                      dominantBaseline="middle"
+                                    >
+                                      <tspan
+                                        x={viewBox.cx}
+                                        y={viewBox.cy}
+                                        className="fill-foreground text-4xl font-bold"
+                                      >
+                                        {item.chartData[0].quantity.toLocaleString()}
+                                        /70
+                                      </tspan>
+                                      <tspan
+                                        x={viewBox.cx}
+                                        y={(viewBox.cy || 0) + 24}
+                                        className="fill-muted-foreground"
+                                      >
+                                        People
+                                      </tspan>
+                                    </text>
+                                  );
+                                }
+                              }}
+                            />
+                          </PolarRadiusAxis>
+                        </RadialBarChart>
+                      </ChartContainer>
+                    </div>
+                  </CardContent>
+                  <div className="col-span-2">
+                    <div className="grid grid-cols-2 gap-4 text-sm  mx-6 mb-6">
+                      {departments.map((item, index) => (
+                        <div
+                          className="flex justify-between gap-2"
+                          key={index + "Letgo"}
+                        >
+                          <div className="cursor-pointer hover:text-blue-500 transition-all active:scale-95">
+                            {item.department_name}
+                          </div>
+                          <div className="flex items-start gap-1 font-bold">
+                            25/32
+                            <span>คน</span>
+                          </div>
+                        </div>
+                      ))}
+                    </div>
+                  </div>
+                </Card>
+              </motion.div>
+            ))}
+        </div>
+        {/* <h2 className="text-xl font-bold">ผลการประเมินตอนนี้</h2> */}
+        <div className="flex flex-wrap gap-5">
+          {myscore.map((item, index) => (
+            <motion.div
+              key={item.id}
+              className="flex-1 py-3 px-7 bg-white shadow-md rounded-2xl"
+              initial={{ opacity: 0, scale: 0.5 }}
+              animate={{ opacity: 1, scale: 1 }}
+              transition={{
+                duration: 0.8,
+                delay: index * 0.1,
+                ease: [0, 0.71, 0.2, 1.01],
+              }}
+            >
+              <div className="flex items-center gap-3">
+                <div
+                  className={`p-2 rounded-lg text-white`}
+                  style={{ backgroundColor: item.color }}
                 >
-                  <RadialBarChart
-                    data={chartData}
-                    endAngle={120}
-                    innerRadius={80}
-                    outerRadius={140}
-                  >
-                    <PolarGrid
-                      gridType="circle"
-                      radialLines={false}
-                      stroke="none"
-                      className="first:fill-muted last:fill-background"
-                      polarRadius={[86, 74]}
-                    />
-                    <RadialBar
-                      dataKey="visitors"
-                      background
-                      className="fill-blue-400"
-                    />
-                    <PolarRadiusAxis
-                      tick={false}
-                      tickLine={false}
-                      axisLine={false}
-                    >
-                      <Label
-                        content={({ viewBox }) => {
-                          if (viewBox && "cx" in viewBox && "cy" in viewBox) {
-                            return (
-                              <text
-                                x={viewBox.cx}
-                                y={viewBox.cy}
-                                textAnchor="middle"
-                                dominantBaseline="middle"
-                              >
-                                <tspan
-                                  x={viewBox.cx}
-                                  y={viewBox.cy}
-                                  className="fill-foreground text-4xl font-bold"
-                                >
-                                  {chartData[0].visitors.toLocaleString()}
-                                </tspan>
-                                <tspan
-                                  x={viewBox.cx}
-                                  y={(viewBox.cy || 0) + 24}
-                                  className="fill-muted-foreground"
-                                >
-                                  People
-                                </tspan>
-                              </text>
-                            );
-                          }
-                        }}
-                      />
-                    </PolarRadiusAxis>
-                  </RadialBarChart>
-                </ChartContainer>
-              </CardContent>
-              <CardFooter className="flex-col gap-2 text-sm">
-                <div className="flex items-center gap-2 font-medium leading-none">
-                  Trending up by 5.2% this month{" "}
-                  <TrendingUp className="h-4 w-4" />
+                  {item.icon}
                 </div>
-                <div className="leading-none text-muted-foreground">
-                  Showing total visitors for the last 6 months
+                <h2 className="text-lg font-semibold">{item.name}</h2>
+              </div>
+              <div className="flex items-center justify-between gap-3 mt-2">
+                <div className="flex items-end gap-3">
+                  <h2 className="font-light">
+                    <span className="text-xl font-medium">{item.now}/</span>
+                    {item.total}
+                  </h2>
+                  <h2 className="text-[12px] font-bold text-emerald-500">
+                    Today
+                  </h2>
                 </div>
-              </CardFooter>
-            </Card>
+                <div className="w-[40px] font-bold">
+                  <CircularProgressbar
+                    value={item.state}
+                    text={`${item.state}%`}
+                    strokeWidth={15}
+                    styles={buildStyles({
+                      // Whether to use rounded or flat corners on the ends - can use 'butt' or 'round'
+                      strokeLinecap: "butt",
+                      pathColor: `${item.color}`,
+                      // Text size
+                      textSize: "30px",
+                      // How long animation takes to go from one percentage to another, in seconds
+                      pathTransitionDuration: 0.5,
+
+                      // Can specify path transition in more detail, or remove it entirely
+                      // pathTransition: 'none',
+                      // Colors
+                      textColor: "#000000",
+                      trailColor: "#D1ECEC",
+                      backgroundColor: "#3e98c7",
+                    })}
+                  />
+                </div>
+              </div>
+              <div className="mt-3">
+                <hr />
+                <h2 className="text-[12px] text-neutral-500 mt-3">
+                  1.3% Performance up
+                </h2>
+              </div>
+            </motion.div>
           ))}
         </div>
-        {myscore.map((item, index) => (
-          <motion.div
-            key={item.id}
-            className="flex-1 py-3 px-7 bg-white shadow-md rounded-2xl"
-            initial={{ opacity: 0, scale: 0.5 }}
-            animate={{ opacity: 1, scale: 1 }}
-            transition={{
-              duration: 0.8,
-              delay: index * 0.1,
-              ease: [0, 0.71, 0.2, 1.01],
-            }}
-          >
-            <div className="flex items-center gap-3">
-              <div
-                className={`p-2 rounded-lg text-white`}
-                style={{ backgroundColor: item.color }}
-              >
-                {item.icon}
-              </div>
-              <h2 className="text-lg font-semibold">{item.name}</h2>
-            </div>
-            <div className="flex items-center justify-between gap-3 mt-2">
-              <div className="flex items-end gap-3">
-                <h2 className="font-light">
-                  <span className="text-xl font-medium">{item.now}/</span>
-                  {item.total}
-                </h2>
-                <h2 className="text-[12px] font-bold text-emerald-500">
-                  Today
-                </h2>
-              </div>
-              <div className="w-[40px] font-bold">
-                <CircularProgressbar
-                  value={item.state}
-                  text={`${item.state}%`}
-                  strokeWidth={15}
-                  styles={buildStyles({
-                    // Whether to use rounded or flat corners on the ends - can use 'butt' or 'round'
-                    strokeLinecap: "butt",
-                    pathColor: `${item.color}`,
-                    // Text size
-                    textSize: "30px",
-                    // How long animation takes to go from one percentage to another, in seconds
-                    pathTransitionDuration: 0.5,
-
-                    // Can specify path transition in more detail, or remove it entirely
-                    // pathTransition: 'none',
-                    // Colors
-                    textColor: "#000000",
-                    trailColor: "#D1ECEC",
-                    backgroundColor: "#3e98c7",
-                  })}
-                />
-              </div>
-            </div>
-            <div className="mt-3">
-              <hr />
-              <h2 className="text-[12px] text-neutral-500 mt-3">
-                1.3% Performance up
-              </h2>
-            </div>
-          </motion.div>
-        ))}
       </div>
       <motion.div
         className="bg-white rounded-2xl shadow-md"
